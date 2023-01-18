71dd8b2da5003e5be3d5d66bc891775992d3fae9
kevin.lemarie <kevin.l@now.online>
2023-01-18 21:39:23 +0100

    remove loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,14 @@ import React, { useEffect, useState } from "react";
 import NavBar from "../Components/navbar/index";
 import styles from "../styles/Home.module.scss";
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    console.log("is loading");
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  }, []);
+  // useEffect(() => {
+  //   console.log("is loading");
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 1000);
+  // }, []);
 
   if (isLoading) {
     return (
@@ -58,15 +58,6 @@ export default function Home() {
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-7EKL12VYBT"
         ></Script>
-        {`
-     
-      
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-7EKL12VYBT');
-       `}
 
         <main className={styles.mainContainer}>
           <>Loading</>
@@ -83,7 +74,38 @@ export default function Home() {
           name="description"
           content="Kévin Lemarié , développeur frond-end javascript ( react js , next js , node js , react native)"
         ></meta>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-7EKL12VYBT`}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7EKL12VYBT', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NM9HGWZ');
+      `}
+      </Script>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-7EKL12VYBT"
+      ></Script>
+
       <main className={styles.mainContainer}>
         <NavBar />
         <Hero />
